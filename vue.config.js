5cf6d5dca6ef600cbe9e9a842d20b305dcde4319
Ia Kalandia <iakalandia1@gmail.com>
2025-03-07 00:14:39 +0400

    Initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/your-repo-name/'  // Replace with your repository name
+        ? '/lazi-app/'  // Replace with your repository name
         : '/'
 } 
