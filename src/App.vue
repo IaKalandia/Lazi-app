775a1e2e50e94f6e552bd281188d8662983e9d46
Ia Kalandia <93795395+IaKalandia@users.noreply.github.com>
2025-03-20 15:27:18 +0400

    Update App.vue
    test

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
                         class="menu-button">
                     <span class="menu-icon"></span>
                 </button>
-
+'
                 <!-- Navigation Links -->
                 <div class="nav-links" :class="{ 'mobile-links': isMobile, 'show': isMenuOpen }">
                     <router-link to="/" custom v-slot="{ navigate }">
